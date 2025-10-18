a3a9ca2d003013f3070df0f88fb4e771afef7763
destiny <desto4q@gmail.com>
2025-10-18 04:44:51 +0100

    fix build fail

--- app/routes/movie/index.tsx
+++ app/routes/movie/index.tsx
@@ -1,6 +1,5 @@
 import TestPlayer from "@/components/TestPlayer.client";
 import { Suspense } from "react";
-import blur from "/blur_2.png";
 
 export default function Index() {
   return <div></div>;
